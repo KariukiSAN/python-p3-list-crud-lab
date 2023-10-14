729cbf22c8732e63c2283fa230912179b7783421
KariukiSAN <kariukian1@gmail.com>
2023-10-14 15:00:30 +0300

    R6

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -18,7 +18,9 @@ def remove_element_from_end_of_list(l):
         return l
 
 def remove_element_from_start_of_list(l):
-    return None
+    if l:
+        l.pop(0)
+        return l
 
 def retrieve_first_element_from_list(l):
     return None
