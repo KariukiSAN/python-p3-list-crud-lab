5d7deae786ca71fcc117d9448608cfb45b1bdba0
KariukiSAN <kariukian1@gmail.com>
2023-10-14 14:38:22 +0300

    R2

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -2,7 +2,7 @@ def create_an_empty_list():
     return []
 
 def create_a_list():
-    return None
+    return ['John', 'Michael', 'Terry', 'Graham']
 
 def add_element_to_end_of_list(l, element):
     return None
